e6334b770355e36fde70235fcdb2304ad295ff48
espuleta0@gmail.com <espuleta0@gmail.com@6c744bdd-cd65-c1e8-b4f0-ae9d8308cc6f>
2011-10-27 00:59:08 +0000

    -Correções do repositório

---
+++ trunk/Project/Virtual_COM_Port/MDK-ARM/STM3210E-EVAL/stm32f10x_sdio.d
@@ -0,0 +1,14 @@
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\src\stm32f10x_sdio.c
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_sdio.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: F:\Keil\ARM\RV31\INC\stdint.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\inc\stm32f10x_conf.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_exti.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_flash.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_usart.h
+.\STM3210E-EVAL\stm32f10x_sdio.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
---
+++ trunk/Project/Virtual_COM_Port/MDK-ARM/STM3210E-EVAL/system_stm32f10x.d
@@ -0,0 +1,13 @@
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.c
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
+.\STM3210E-EVAL\system_stm32f10x.o: F:\Keil\ARM\RV31\INC\stdint.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\inc\stm32f10x_conf.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_exti.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_flash.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_usart.h
+.\STM3210E-EVAL\system_stm32f10x.o: ..\..\..\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
